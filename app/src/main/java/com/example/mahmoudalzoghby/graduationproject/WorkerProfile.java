78709a433b370af5b9e06f32ef332bcc205597d4
MahmoudAlzoghbi <mahmodalzoghby111@gmail.com>
2019-03-12 14:33:41 +0200

    first commit

---
+++ app/src/main/java/com/example/mahmoudalzoghby/graduationproject/WorkerProfile.java
@@ -0,0 +1,13 @@
+package com.example.mahmoudalzoghby.graduationproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkerProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worker_profile);
+    }
+}
